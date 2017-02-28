911bdd5a8088a38e2edd1091f95a8e04eeedf218
matthieuMoisson <matthieu.moisson38@gmail.com>
2017-02-28 09:15:31 +0100

    premier commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,11 @@
 package sample;
 
+
+import javafx.event.ActionEvent;
+
 public class Controller {
+
+    public void clickButton(ActionEvent event){
+        System.out.println("Hello world !!");
+    }
 }
