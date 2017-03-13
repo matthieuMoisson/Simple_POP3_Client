3a49a92f0b36e857212aff7910d42992ed4baa56
Jensen Joymangul <jensen.joymangul@etu.univ-lyon1.fr>
2017-03-13 21:02:41 +0100

    Send encrypted password for encryption

--- src/transaction/Authentication.java
+++ src/transaction/Authentication.java
@@ -3,6 +3,9 @@ package transaction;
 import sample.Connexion;
 import sample.Message;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by Gaetan on 07/03/2017.
  * Authentication
@@ -12,18 +15,38 @@ public class Authentication extends Transaction{
     private String username;
     private String password;
     private String timestamp;
-    private final String SHARE_SECRET = "montagne";
+    private String encodedPassword;
 
     public Authentication(Connexion connexion, String username, String password, String timestamp) {
         super(connexion);
         this.username = username;
         this.password = password;
         this.timestamp = timestamp;
+        this.encodedPassword = this.getEncodedPassword(this.password);
+    }
+
+    public String getEncodedPassword(String password)
+    {
+        MessageDigest messageDigest;
+        try {
+            String fullPhrase = this.timestamp + password;
+            messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.update(fullPhrase.getBytes());
+            byte[] messageDigestMD5 = messageDigest.digest();
+            StringBuilder stringBuffer = new StringBuilder();
+            for (byte bytes : messageDigestMD5) {
+                stringBuffer.append(String.format("%02x", bytes & 0xff));
+            }
+            return stringBuffer.toString();
+        } catch (NoSuchAlgorithmException exception) {
+            exception.printStackTrace();
+        }
+        return null;
     }
 
     @Override
     public void run() {
-        this.connexion.send(new Message(Command.APOP, username + " " + password));
+        this.connexion.send(new Message(Command.APOP, username + " " + encodedPassword));
         this.message = this.connexion.receive();
         setChanged();
         notifyObservers(this);
